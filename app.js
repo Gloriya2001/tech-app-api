f0bbc9de5a23f81f1eb394f5d4d7bbc1eb6267f1
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-12 23:54:49 +0530

    add api done

--- app.js
+++ app.js
@@ -2,33 +2,30 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 
+const { techmodel } = require("./models/tech")
+
 const app = express()
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 
-app.post("/add",(req,res)=>{
+app.post("/add", (req, res) => {
 
-res.send("Hai")
+    let input = req.body
+    let tech =  new techmodel(input)
+    tech.save()
+    console.log(tech)
+    res.json({ "status": "sucess" })
+
 
 })
 
 
 
 
-app.post("/search",(req,res)=>{
+app.post("/search", (req, res) => {
 
-res.send("hello")
-
-})
-
-
-
-
-
-app.post("/delete",(req,res)=>{
-
-res.send("Success")
+    res.send("hello")
 
 })
 
@@ -36,14 +33,24 @@ res.send("Success")
 
 
 
-app.post("/view",(req,res)=>{
-res.send("view")
+app.post("/delete", (req, res) => {
+
+    res.send("Success")
 
 })
 
 
 
-app.listen(8080,(req,res)=>{
+
+
+app.post("/view", (req, res) => {
+    res.send("view")
+
+})
+
+
+
+app.listen(8080, (req, res) => {
 
     console.log("server started")
 
